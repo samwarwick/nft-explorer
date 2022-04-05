40f13e5bde04686625307bb9440e5cad5c119a34
Sam Warwick <sam@samwarwick.com>
2022-04-05 10:25:36 +1000

    Add React template

---
+++ src/setupTests.js
@@ -0,0 +1,17 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom';
+
+// https://stackoverflow.com/questions/71685441/react-testing-library-gives-console-error-for-reactdom-render-in-react-18
+
+const originalError = console.error;
+beforeAll(() => {
+  console.error = (...args) => {
+    if (/Warning: ReactDOM.render is no longer supported in React 18./.test(args[0])) {
+      return;
+    }
+    originalError.call(console, ...args);
+  };
+});
