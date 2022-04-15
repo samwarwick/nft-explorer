ec31b2293430705dc3742d4fba802c1471942cd1
samwarwick <samwarwick@users.noreply.github.com>
2022-04-15 03:44:44 +0000

    deploy: a5a4219258cbcaf320dc40ebb76724a0c5f1976b

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('Shows the NFT explorer', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/NFT Explorer/i);
-  expect(linkElement).toBeInTheDocument();
-});
